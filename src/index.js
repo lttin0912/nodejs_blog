89223bd623423161d9659584a6c10dcd71dff104
Lý Tự Tín <lttin0912@gmail.com>
2020-08-07 10:31:07 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
-const path = require('path')
-const express = require('express')
-const handlebars = require('express-handlebars')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({
-    extended: true
-}))
-app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 // HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 // Template Engine
-app.engine('hbs', handlebars({
-    extname: '.hbs'
-}))
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources/views'))
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
-route(app)
+route(app);
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`),
+);

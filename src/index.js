96b5d8cdf97cb90205fdd55f99be686809c3b12b
Lý Tự Tín <lttin0912@gmail.com>
2020-08-13 11:39:33 +0700

    create, list, edit courses

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ const path = require('path');
 const express = require('express');
 const handlebars = require('express-handlebars');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const app = express();
 const port = 3000;
 
 const route = require('./routes');
+
+// Connect MongoDB
+const db = require('./config/db');
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
@@ -15,6 +20,8 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+
 // HTTP logger
 app.use(morgan('combined'));
 
@@ -23,14 +30,17 @@ app.engine(
     'hbs',
     handlebars({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () =>
-    console.log(`Example app listening at http://localhost:${port}`),
+    console.log(`App listening at http://localhost:${port}`),
 );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 const newsRouter = require('./news');
+const coursesRouter = require('./course');
 const siteRouter = require('./site');
+const meRouter = require('./me');
 
 function route(app) {
+    app.use('/me', meRouter);
     app.use('/news', newsRouter);
+    app.use('/courses', coursesRouter);
     app.use('/', siteRouter);
 }
 

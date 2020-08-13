f2c243b68ab599c5c83abe36d555676a7665ebb2
Lý Tự Tín <lttin0912@gmail.com>
2020-08-13 23:15:55 +0700

    delete course

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,13 @@ const siteRouter = require('./site');
 const meRouter = require('./me');
 
 function route(app) {
+    app.use(function (req, res, next) {
+        if (req.query._method == 'DELETE') {
+            req.method = 'DELETE';
+            req.url = req.path;
+        }
+        next();
+    });
     app.use('/me', meRouter);
     app.use('/news', newsRouter);
     app.use('/courses', coursesRouter);
